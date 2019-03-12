a5bae5f0de9f87b38ef16d5329ce46bc00d68231
Anastasia Shapovalova <shabalinastya@gmail.com>
2019-03-12 23:55:56 +0000

    First homework

--- HW_1/main.js
+++ HW_1/main.js
@@ -1,68 +1,56 @@
 const fs = require('fs');
 const path = require('path');
 
-const base = './test';
+const base = process.argv[2]||'./test';
+const result = process.argv[3]||'./temp/subtemp/';
+const removeBase = process.argv[4]||true;
 
 const copyFile = (file) => {  
 
   const fileName = path.parse(file).base
   const newFolderName =  fileName.charAt(0).toUpperCase()
 
-  if (!fs.existsSync('./temp/'+newFolderName)) {
-    console.log('created')
-    fs.mkdirSync('./temp/'+newFolderName, { recursive: true });
+  if (!fs.existsSync(result+newFolderName)) {
+    fs.mkdirSync(result+newFolderName, { recursive: true });
   } 
 
-  fs.link(file, path.join('./temp/',newFolderName ,fileName), err => {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
-    console.log('copied')
-    fs.unlink(file, err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(file + ' deleted')
-      fs.rmdir(path.parse(file).dir, err => {
-        if (err) {
-          console.log(err);
-          return;
-        }
+  console.log("start linkSync file " + file);
+  fs.linkSync(file, path.join(result,newFolderName ,fileName));
+  
+    console.log("Start deleting " + file);
+    console.log("rb " + removeBase);
+    if (removeBase){
+      fs.unlinkSync(file);
+      
         console.log('Delete done! ' + file);
-      });
-    })
-  });  
-
-  console.log(file)  
-  /*
-  fs.unlink(file, err => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-  })*/
+    }   
 }
 
-const readDir = (base, level) => {
-  const files = fs.readdirSync(base);
+const readDir = (base, level, callback) => {
+  const files = fs.readdirSync(base);  
 
   files.forEach(item => {
     let localBase = path.join(base, item);
     let state = fs.statSync(localBase);
     if (state.isDirectory()) {
       console.log(' '.repeat(level) + 'DIR: ' + item);
-      readDir(localBase, level + 1);
+      readDir(localBase, level + 1, removeBase => {
+        if (removeBase){
+          fs.rmdirSync(localBase);
+            console.log('Delete done! ' + localBase);
+        }
+      });
+      console.log("Start deleting " + localBase);
     } else {
       console.log(' '.repeat(level) + 'File: ' + item);
       copyFile(localBase)          
     }
-  })
-  
+  });
+  callback(removeBase);
   
 }
 
-readDir(base, 0);
+readDir(base, 0, tmp => {});
+fs.rmdirSync(base);
 
 
